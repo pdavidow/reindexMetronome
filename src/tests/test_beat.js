bfced42cff0af74569d206a0999d503f14920b86
Paul Davidowitz <pdavidow@fastmail.fm>
2016-07-05 19:43:16 -0400

    minor cleanup

--- src/tests/test_beat.js
+++ src/tests/test_beat.js
@@ -25,15 +25,15 @@ test('Beat rh tick indices', (assert) => {
 
     let actual = Beat({rh: 3, lh: 1}).rhTickIndices();
     let expected = [0,1,2];
-    assert.same(actual, expected, msg);
+    assert.isEquivalent(actual, expected, msg);
 
     actual = Beat({rh: 3, lh: 4}).rhTickIndices();
     expected = [0,4,8];
-    assert.same(actual, expected, msg);
+    assert.isEquivalent(actual, expected, msg);
 
     actual = Beat({rh: 8, lh: 6}).rhTickIndices();
     expected = [0,3,6,9,12,15,18,21];
-    assert.same(actual, expected, msg);
+    assert.isEquivalent(actual, expected, msg);
 
     assert.end();
 });
@@ -43,15 +43,15 @@ test('Beat lh tick indices', (assert) => {
 
     let actual = Beat({rh: 3, lh: 1}).lhTickIndices();
     let expected = [0];
-    assert.same(actual, expected, msg);
+    assert.isEquivalent(actual, expected, msg);
 
     actual = Beat({rh: 3, lh: 4}).lhTickIndices();
     expected = [0,3,6,9];
-    assert.same(actual, expected, msg);
+    assert.isEquivalent(actual, expected, msg);
 
     actual = Beat({rh: 8, lh: 6}).lhTickIndices();
     expected = [0,4,8,12,16,20];
-    assert.same(actual, expected, msg);
+    assert.isEquivalent(actual, expected, msg);
 
     assert.end();
 });
